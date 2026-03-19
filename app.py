967bfa461320415d9eb16b70482d8ae823a5bd2d
manucitavm-web <manucitavm@gmail.com>
2026-03-19 14:18:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ if img_file_buffer is not None:
     prediction = model.predict(data)
     print(prediction)
     if prediction[0][0]>0.5:
-      st.header('Manu'+str( prediction[0][0]) )
+      st.header('Sin Manu'+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
-      st.header('Majo'+str( prediction[0][1]))
+      st.header('Manu'+str( prediction[0][1]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
